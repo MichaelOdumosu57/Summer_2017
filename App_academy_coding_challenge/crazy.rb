bb2d37f78de1a5eb27186730254cced4338efafa
Michael Odumosu <michaelodumosu57@gmail.com>
2017-07-27 23:02:32 -0400

    trying to get proper division to work

--- App_academy_coding_challenge/crazy.rb
+++ App_academy_coding_challenge/crazy.rb
@@ -4,16 +4,24 @@ def crazy_nums(num)
 	count = 0
 	by_three = 0
 	by_five = 0
+	spill = 0
 	arr = []
 	while count != num
 		by_three  = count % 3
+		by_five = count % 5
 		if by_three == 0
-			by_five = count % 5
+			
 			if by_five == 0
+				spill = 1
 				end
 			arr.push(count)
 			end
-		
+		if by_five == 0
+			if spill == 1
+				arr.pop				
+				end
+			arr.push(count)
+			end		
 		count = count + 1
 		end
 puts(arr)
