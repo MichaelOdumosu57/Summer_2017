089ebf0ec6267096d083852333beff9c1a50f95f
Michael Odumosu <michaelodumosu57@gmail.com>
2017-07-27 22:39:43 -0400

    Got squre nums running

--- App_academy_coding_challenge/square_nums.rb
+++ App_academy_coding_challenge/square_nums.rb
@@ -1,13 +1,14 @@
 puts("Put in a number to find the max square")
-square = gets()
+square = gets
 
 def square_nums(num) 
 	side = 1
-	count = 0
+	count = -1
 	total = 0
 	while total < num
 		total = side * side
 		count = count + 1
+		side = side + 1
 	end
 	puts(count)
 end
