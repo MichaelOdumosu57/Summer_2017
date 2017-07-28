255635a76473bc78989bef2ef416e61f27e70b4a
Michael Odumosu <michaelodumosu57@gmail.com>
2017-07-27 22:30:41 -0400

    Fished 1st part

--- App_academy_coding_challenge/crazy_sum.rb
+++ App_academy_coding_challenge/crazy_sum.rb
@@ -1,6 +1,7 @@
 arr = []
 input = 0
 check = "Isev"
+sum = 0
 puts("Enter 'DONE' if you are done")
 while check != "DONE"
 	input = gets()
@@ -9,6 +10,20 @@ while check != "DONE"
 		arr.push(input)
 		end
 end
-puts(arr)
 
 
+def crazy_sum(jumble, total)
+	counter = 1
+	puts(jumble.length)
+	while counter != jumble.length
+		total = total + ( counter * jumble[counter].to_i)
+		counter = counter + 1
+		puts("totoal so far")
+		puts(total)
+		puts("counter")
+		puts(counter)
+		end
+	puts(total)
+end
+puts("this is the answer")
+crazy_sum(arr,sum)
